b684d937f76c01d16992559bd89c48220c472b7a
timurvvolkov <timurvvolkov@gmail.com>
2021-08-10 02:47:38 +0300

    08 4 Классы и Файлы в Java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Goodbye");
+        System.out.println("Hello world");
     }
 }
---
+++ src/lesson2_1/Animal.java
@@ -0,0 +1,14 @@
+package lesson2_1;
+
+public class Animal {
+    String name = "Barsik";
+
+    public void setName(String newName) {
+        name = newName;
+    }
+}
+ class Zooceeper {
+     public static void main(String[] args) {
+         System.out.println("Hello");
+     }
+ }
