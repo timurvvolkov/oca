4397fd0ae48ee8d2bfd64502b802571beae9b61f
timurvvolkov <timurvvolkov@gmail.com>
2021-11-11 22:54:23 +0300

    39 9 Hiding vs Overriding методов в Java

---
+++ src/lesson6_1/methods/OverridingVsHiding.java
@@ -0,0 +1,30 @@
+package lesson6_1.methods;
+
+public class OverridingVsHiding {
+}
+
+class Marsupial {
+    public static boolean isBiped() {
+        return false;
+    }
+
+    public void getMarsupialDescription() {
+        System.out.println("Marsupial walks on two legs: " + isBiped());
+    }
+}
+
+class Kangaroo extends Marsupial {
+    public static boolean isBiped() {
+        return true;
+    }
+
+    public void getKangarooDescription() {
+        System.out.println("Kangaroo hops on two legs: " + isBiped());
+    }
+
+    public static void main(String[] args) {
+        Kangaroo joey = new Kangaroo();
+        joey.getMarsupialDescription();
+        joey.getKangarooDescription();
+    }
+}
