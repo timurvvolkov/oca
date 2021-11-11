f9008ebd5db05a469b3185aba439791de3ab8850
timurvvolkov <timurvvolkov@gmail.com>
2021-11-11 22:56:30 +0300

    39 10 final методы в Java

---
+++ src/lesson6_1/methods/FinalMethods.java
@@ -0,0 +1,16 @@
+package lesson6_1.methods;
+
+public class FinalMethods {
+}
+
+class Bird {
+    public final boolean hasFeathers() {
+        return true;
+    }
+}
+
+class Penguin extends Bird {
+//    public boolean hasFeathers() {
+//        return false;
+//    }
+}
