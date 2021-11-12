10474ee4d34de90d5c47da25b82b31151dc8c06b
timurvvolkov <timurvvolkov@gmail.com>
2021-11-13 00:50:46 +0300

    40 4 Правила объявления абстрактного класса в Java

---
+++ src/lesson6_2/ext/Animal.java
@@ -0,0 +1,11 @@
+package lesson6_2.ext;
+
+public abstract class Animal {
+    public abstract String getName();
+    public String getAge() {
+        return "";
+    }
+}
+
+//class Walrus extends Animal { }
+//abstract class Eagle extends Animal { }
