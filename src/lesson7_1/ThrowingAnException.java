7060f54337331766be10a7f122c3d600a0b1b252
timurvvolkov <timurvvolkov@gmail.com>
2021-11-23 01:25:23 +0300

    43 3 Проброс Исключения в Java

---
+++ src/lesson7_1/ThrowingAnException.java
@@ -0,0 +1,8 @@
+package lesson7_1;
+
+public class ThrowingAnException {
+    public static void main(String[] args) {
+        String[] animals = new String[0];
+        System.out.println(animals[0]);
+    }
+}
