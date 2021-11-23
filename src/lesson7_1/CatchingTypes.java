acaedfa867be71374f4ba100932c14f18ec636b9
timurvvolkov <timurvvolkov@gmail.com>
2021-11-24 00:07:45 +0300

    44 3 Отлов ошибок с разными типами в Java

---
+++ src/lesson7_1/CatchingTypes.java
@@ -0,0 +1,47 @@
+package lesson7_1;
+
+public class CatchingTypes {
+
+    void seeAnimal() {}
+
+    public void visitPorcupine() {
+        try {
+            seeAnimal();
+        } catch (AnimalsOutForAWalk e) {
+            System.out.println("try back later");
+        } catch (ExhibitClosed e) {
+            System.out.println("not today");
+        }
+    }
+
+    class AnimalsOutForAWalk extends RuntimeException {}
+    class ExhibitClosed extends RuntimeException {}
+    class ExhibitClosedForLunch extends ExhibitClosed {}
+
+    class VisitAnimals {
+
+        void seeAnimal() {}
+
+        public void visitMonkeys() {
+            try {
+                seeAnimal();
+            } catch (ExhibitClosedForLunch e) {
+                System.out.println("try back later");
+            } catch (ExhibitClosed e) {
+                System.out.println("not today");
+            }
+        }
+
+        public void visitSnakes() {
+            try {
+                seeAnimal();
+            } catch (ExhibitClosed e) {
+                System.out.println("not today");
+            } catch (RuntimeException e) {
+                System.out.println("runtime exception");
+            } catch (Exception e) {
+                System.out.println("exception");
+            }
+        }
+    }
+}
