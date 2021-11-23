b339e8b2a99feeda10eae9ef7b7b852c5f7cf1c8
timurvvolkov <timurvvolkov@gmail.com>
2021-11-24 00:17:19 +0300

    44 4 Несколько исключений в Java

--- src/lesson7_1/CatchingTypes.java
+++ src/lesson7_1/CatchingTypes.java
@@ -13,35 +13,35 @@ public class CatchingTypes {
             System.out.println("not today");
         }
     }
+}
 
-    class AnimalsOutForAWalk extends RuntimeException {}
-    class ExhibitClosed extends RuntimeException {}
-    class ExhibitClosedForLunch extends ExhibitClosed {}
+class AnimalsOutForAWalk extends RuntimeException {}
+class ExhibitClosed extends RuntimeException {}
+class ExhibitClosedForLunch extends ExhibitClosed {}
 
-    class VisitAnimals {
+class VisitAnimals {
 
-        void seeAnimal() {}
+    void seeAnimal() {}
 
-        public void visitMonkeys() {
-            try {
-                seeAnimal();
-            } catch (ExhibitClosedForLunch e) {
-                System.out.println("try back later");
-            } catch (ExhibitClosed e) {
-                System.out.println("not today");
-            }
+    public void visitMonkeys() {
+        try {
+            seeAnimal();
+        } catch (ExhibitClosedForLunch e) {
+            System.out.println("try back later");
+        } catch (ExhibitClosed e) {
+            System.out.println("not today");
         }
+    }
 
-        public void visitSnakes() {
-            try {
-                seeAnimal();
-            } catch (ExhibitClosed e) {
-                System.out.println("not today");
-            } catch (RuntimeException e) {
-                System.out.println("runtime exception");
-            } catch (Exception e) {
-                System.out.println("exception");
-            }
+    public void visitSnakes() {
+        try {
+            seeAnimal();
+        } catch (ExhibitClosed e) {
+            System.out.println("not today");
+        } catch (RuntimeException e) {
+            System.out.println("runtime exception");
+        } catch (Exception e) {
+            System.out.println("exception");
         }
     }
 }
